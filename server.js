e67963e43ecd873c34f87ffec6246ca08c46412f
Robbie Jaeger <jaeger.rob@gmail.com>
2021-12-21 13:59:39 -0700

    Initialize list notifications to be sent

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const app = express();
 
 app.set('port', process.env.PORT || 3001);
 
+const notifications = [{
+  username: 'example@example.com',
+  text: 'Make the most of your life.',
+  sendDate: Date.now()
+}];
+
+
+
 app.listen(app.get('port'), () => {
   console.log(`Scheduler test running on http://localhost:${app.get('port')}`);
 });
