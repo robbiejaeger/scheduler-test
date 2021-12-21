27d77cbd9e289cdecc4609e07af4c3e8e3d4a442
Robbie Jaeger <jaeger.rob@gmail.com>
2021-12-21 14:23:03 -0700

    Create basic scheduler going through list of notifications

--- server.js
+++ server.js
@@ -5,14 +5,28 @@ const app = express();
 
 app.set('port', process.env.PORT || 3001);
 
-const notifications = [{
-  username: 'example@example.com',
-  text: 'Make the most of your life.',
-  sendDate: Date.now()
-}];
+const notifications = [
+  {
+    username: 'example@example.com',
+    text: 'Make the most of your life.',
+    sendDate: Date.now()
+  },
+  {
+    username: 'example@example.com',
+    text: 'Just do it.',
+    sendDate: Date.now() + 10000
+  }
+];
 
-cron.schedule('1-59/2 * * * * *', () => {
+cron.schedule('0-59/2 * * * * *', () => {
   console.log('Cron: Running a task every 2 seconds');
+  const currentTime = Date.now();
+  notifications.forEach(notification => {
+    if (currentTime > notification.sendDate) {
+      console.log(`${notification.username}: ${notification.text}`);
+      notifications.shift();
+    }
+  });
 });
 
 app.listen(app.get('port'), () => {
